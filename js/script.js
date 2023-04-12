01a64ee1857776ce1f2c315da4088e98794d4b08
Lida Jiang <ljiang38@id.iit.edu>
2023-04-11 20:12:14 -0500

    js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
+var button = document.getElementById('elementid')
 
+function ShowText (){
+  var textbox = document.getElementById('enterinput')
+  document.getElementById(elementid).innerHTML=textbox.ariaValueMax;
+  alert("Lida Jiang:" + textbox.value);
+}
 
-
+button.addEventListener(click, ShowText);
 
 
 
