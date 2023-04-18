47fb748a505990daab767eb8005872d3d1497826
lidajiang19 <98130808+lidajiang19@users.noreply.github.com>
2023-04-18 14:44:16 -0500

    update script.js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ function init(){
 //add your javascrip between these two lines of code
 var button = document.getElementById('elementid')
 
-function ShowText(){
+function showText(){
   var textbox = document.getElementById('enterinput')
   document.getElementById(elementid).innerHTML=textbox.value
   alert('Lida Jiang:' + textbox.value)
 }
 
-button.addEventListener(click, ShowText)
+button.addEventListener('click', showText)
 
 
 
